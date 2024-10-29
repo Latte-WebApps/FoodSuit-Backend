76b894d8320a3ab602686fb3401a12e77b635be7
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-29 17:49:07 -0500

    feat: add interfaces to shared direc

---
+++ FoodSuit-Backend/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using FoodSuit_Backend.Inventory.Domain.Model.Aggregates;
+using FoodSuit_Backend.Shared.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public abstract class BaseRepository<TEntity>(AppDbContext context) : IBaseRepository<TEntity>
+    where TEntity : class
+{
+    protected readonly AppDbContext Context = context;
+
+    public async Task AddAsync(TEntity entity) => await Context.Set<TEntity>().AddAsync(entity);
+
+    public async Task<IEnumerable<Item>> FindByIdAsync(int id) => (IEnumerable<Item>)await Context.Set<TEntity>().FindAsync(id);
+
+    public void Update(TEntity entity) => Context.Set<TEntity>().Update(entity);
+
+    public void Remove(TEntity entity) => Context.Set<TEntity>().Remove(entity);
+
+    public async Task<IEnumerable<TEntity>> ListAsync() => await Context.Set<TEntity>().ToListAsync();
+}
