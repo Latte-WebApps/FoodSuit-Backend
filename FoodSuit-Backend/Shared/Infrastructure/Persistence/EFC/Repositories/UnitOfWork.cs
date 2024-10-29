dd97e0e3485da85eeab5192b1a8f59bf960f0d83
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-29 17:55:11 -0500

    feat: set shared repositories

---
+++ FoodSuit-Backend/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using FoodSuit_Backend.Shared.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
