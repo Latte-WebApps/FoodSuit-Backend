9ab033bdf56be55d235bfe04be2d29e7a9043b9e
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-29 17:44:16 -0500

    feat: add repository

---
+++ FoodSuit-Backend/Inventory/Infrastructure/Persistence/EFC/Repositories/ItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using FoodSuit_Backend.Inventory.Domain.Model.Aggregates;
+using FoodSuit_Backend.Inventory.Domain.Repositories;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodSuit_Backend.Inventory.Infrastructure.Persistence.EFC.Repositories;
+
+public class ItemRepository(AppDbContext context) : BaseRepository<Item>(context), IItemRepository
+{
+    public new async Task<Item?> FindByIdAsync(int id)
+    {
+        return await Context.Set<Item>()
+            .FirstOrDefaultAsync(i => i.Id == id);
+    }
+
+    public async Task<IEnumerable<Item>> FindByNameAsync(string name)
+    {
+        return await Context.Set<Item>().Where(i => i.Name == name).ToListAsync();
+    }
+
+    public async Task UpdateAsync(Item item)
+    {
+        Context.Set<Item>().Update(item);
+        await Context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Item>> FindAllAsync()
+    {
+        return await Context.Set<Item>().ToListAsync();
+    }
+}
