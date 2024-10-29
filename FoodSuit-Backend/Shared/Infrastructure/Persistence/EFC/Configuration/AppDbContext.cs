271a18cd72f07c7d09f2c251303613f6f88c97fa
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-29 17:51:50 -0500

    add: set AppDbContext

---
+++ FoodSuit-Backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using FoodSuit_Backend.Inventory.Domain.Model.Aggregates;
+using FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodSuit_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<Item>().HasKey(f => f.Id);
+        builder.Entity<Item>().Property(f => f.Id)
+            .IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Item>().Property(f => f.Name)
+            .IsRequired();
+        builder.Entity<Item>().Property(f => f.Quantity)
+            .IsRequired();  
+        builder.Entity<Item>().Property(f => f.Image)
+            .IsRequired();
+        
+        builder.UseSnakeCaseNamingConvention();
+
+    }
+    
+}
