2ce19aae97c745d715872a77648c85d9866892e1
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-29 17:44:55 -0500

    feat: add services

---
+++ FoodSuit-Backend/Inventory/Domain/Services/IItemCommandService.cs
@@ -0,0 +1,12 @@
+﻿using FoodSuit_Backend.Inventory.Domain.Model.Aggregates;
+using FoodSuit_Backend.Inventory.Domain.Model.Commands;
+
+namespace FoodSuit_Backend.Inventory.Domain.Services;
+
+public interface IItemCommandService
+{
+    Task<Item?> Handle(CreateItemCommand command);
+    Task<Item?> Handle(UpdateItemCommand command);
+    Task<bool?> Handle(DeleteItemCommand command);
+    
+}
